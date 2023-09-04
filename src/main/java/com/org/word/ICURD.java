7b4d8a7814e7ab9ee46f6e6d8734f33d461c7e30
jm_Han <22000789@handong.ac.kr>
2023-09-04 20:18:24 +0900

    ICURD interface 및 WordCRUD class 추가

---
+++ src/main/java/com/org/word/ICURD.java
@@ -0,0 +1,9 @@
+package com.org.word;
+
+public interface ICURD {
+    public Object add();
+    public void addItem();
+    public void updateItem();
+    public void deleteItem();
+    public void listAll();
+}
---
+++ src/main/java/com/org/word/WordCURD.java
@@ -0,0 +1,29 @@
+package com.org.word;
+
+public class WordCURD implements ICURD{
+
+    @Override
+    public Object add() {
+        return null;
+    }
+
+    @Override
+    public void addItem() {
+
+    }
+
+    @Override
+    public void updateItem() {
+
+    }
+
+    @Override
+    public void deleteItem() {
+
+    }
+
+    @Override
+    public void listAll() {
+
+    }
+}
